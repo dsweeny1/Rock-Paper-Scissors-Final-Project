8b4404a380dca4e1373f241042667df8bd6c7c89
Danielle Sweeny <dsweeny1@gmail.com>
2022-06-13 16:27:35 -0600

    Update global variable; update naming conventions; add icons to querySelectors; update eventListeners; add eventListeners for icons; update and edit funtions

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Global Variables
-
-var game = new Game(human, type);
+var game;
 
 // QuerySelectors
 var rulesSection = document.querySelector('.rules-section')
@@ -13,25 +12,30 @@ var classicCharacters = document.querySelector('.characters-classic')
 var spicyCharacters = document.querySelector('.characters-spicy')
 var icon = document.querySelector('.icon')
 var changeGameButton = document.querySelector('.change-game-button')
-var computerChoice = document.querySelector('.computer-choice')
-var humanChoice = document.querySelector('.human-choice')
+var computerFighter = document.querySelector('.computer-fighter')
+var humanFighter = document.querySelector('.human-fighter')
 var result = document.querySelector('.result')
 var computerWins = document.querySelector('.computer-wins')
 var humanWins = document.querySelector('.human-wins')
+var rock = document.getElementById('rock')
+var paper = document.getElementById('paper')
+var scissors = document.getElementById('scissors')
+var flower = document.getElementById('flower')
+var mushroom = document.getElementById('mushroom')
 
 // EventListeners
-classicRulesButton.addEventListener("click", goToClassicGame);
-spicyRulesButton.addEventListener("click", goToAdvancedGame);
-changeGameButton.addEventListener("click", returnToHomePage);
+classicRulesButton.addEventListener("click", playClassicGame);
+spicyRulesButton.addEventListener("click", playSpicyGame);
+changeGameButton.addEventListener("click", returnHome);
 classicCharacters.addEventListener("click", playGame);
 spicyCharacters.addEventListener("click", playGame);
+rock.addEventListener('click', viewPlayerChoice)
+paper.addEventListener('click', viewPlayerChoice)
+scissors.addEventListener('click', viewPlayerChoice)
+flower.addEventListener('click', viewPlayerChoice)
+mushroom.addEventListener('click', viewPlayerChoice)
 
 //Functions
-
-// hideElement(chooseFighter);
-// viewElement(displayResults);
-// hideElement(classicCharacters);
-// hideElement(spicyCharacters);
 
 
 // On page load you should see Rock, Paper, Scissors Title; Human title and Human icon, Computer title and Computer icon; Choose Your Game!
@@ -41,7 +45,7 @@ spicyCharacters.addEventListener("click", playGame);
 // At that point the page will default back to the current game selected, either classic or spicy and the User can continue to play that game or click the Change Game Button and select the other game.
 
 // If Classic Game is selected show classicCharacters
-function selectGame() {
+function selectGame(event) {
   if (event.target.id === charactersClassic) {
     viewElement(classicCharacters)
   } else if (event.target.id === charactersSpicy) {
@@ -54,33 +58,19 @@ function selectGame() {
   viewElement(changeGameButton)
 }
 
-// this function is when the user selects rock, paper, scissors, mushroom, or flower.
-//Once the user selects a fighter, Choose Your Figher needs to hide
-function selectFighter() {
-  if (event.target.id === 'rock') {
-    game.human = 'rock';
-  } else if (event.target.id === 'paper') {
-    game.human = 'paper';
-  } else if (event.target.id === 'scissors') {
-    game.human = 'scissors';
-  } else if (event.target.id === 'flower') {
-    game.human = 'flower';
-  } else if (event.target.id === 'mushroom') {
-    game.human = 'mushroom';
-  }
-  // hideElement(chooseGame);
-  // viewElement(changeGameButton);
-  // hideElement(rulesSection)
-}
-
-function playGame() {
-  game.human.takeTurn(event);
-  game.getComputerChoice();
-  game.updatePlayerChoices();
+function playGame(event) {
+  // I think this is where a new instance of Game goes?
+  game = new Game()
+  // var id = event.target.id
+  // console.log(game.humanFighter);
+  game.human.selectHumanFighter()
+  // had id in parends, once I took it out my setTimeout function started working. Still no image upon completion of game
+  game.computer.selectComputerFighter()
   game.determineWinner();
 }
 
-function playClassicGame() {
+function playClassicGame(event) {
+  playGame(event)
   game.type = 'Classic';
   hideElement(displayResults);
   hideElement(result);
@@ -89,10 +79,11 @@ function playClassicGame() {
   hideElement(rulesSection);
   viewElement(classicCharacters);
   viewElement(changeGameButton);
-  game.chooseCharacters();
+  game.showCharacters();
 }
 
-function playSpicyGame() {
+function playSpicyGame(event) {
+  playGame(event)
   game.type = 'Spicy';
   hideElement(displayResults);
   hideElement(result);
@@ -102,13 +93,78 @@ function playSpicyGame() {
   viewElement(classicCharacters);
   viewElement(spicyCharacters);
   viewElement(changeGameButton);
-  game.chooseCharacters();
+  game.showCharacters();
+}
+
+function viewPlayerChoice(humanFighter, computerFighter) {
+  hideElement(chooseFighter);
+  viewElement(displayResults);
+  hideElement(classicCharacters);
+  hideElement(spicyCharacters);
+  if (game.winner === 'Human') {
+    displayResults.innerHTML =
+    `<img class="player-image" src="./assests/player_image.png" width="125" height="125"/>`
+  } else {
+    displayResults.innerHTML =
+    `<img class="player-image" src="./assests/computer_icon.png" width="125" height="125"/>`
+  }
+  // displayWinner.innerHTML = "";
+  // displayWinner.innerHTML += `
+  // <section class="pick humanFighter" id="humanFighter">
+  //     <img id=${humanFighter} src='./assests/${humanFighter}.png'>
+  //   </section>
+  //   <section class="pick computerFighter" id="computerFighter">
+  //     <img id=${computerFighter} src='./assests/${computerFighter}.png'>
+  //   </section>
+  // `;
+
+  // what I want to do here is show either the person image or the computer image, whomever wins + HUMANS WINS!!! or COMPUTER WINS!!!
+  viewElement(result)
+  displayWinner()
+  returnToGame()
+}
+
+function displayWinner() {
+  if (game.winner === 'Human') {
+    game.human.wins += 1;
+    result.innerText = 'HUMAN WINS!!!';
+    humanWins.innerHTML = 'wins: ' + game.human.wins;
+  } else if (game.winner === 'Computer') {
+    game.computer.wins +=1 ;
+    result.innerText = 'COMPUTER WINS!!!';
+  } else {
+    game.winner = 'tie';
+    result.innerText = 'TIE GAME!';
+  }
+  hideElement(classicCharacters);
+  hideElement(spicyCharacters);
+  // viewElement(displayResults);
+  returnToGame()
 }
 
 function returnToGame() {
   if (game.type === 'Classic') {
-    setTimeout(goToClassicGame, 2000);
+    setTimeout(playClassicGame, 10000);
   } else {
-    setTimeout(goToSpicyGame, 2000);
+    setTimeout(playSpicyGame, 10000);
   }
+}
+
+function returnHome() {
+  event.preventDefault;
+  hideElement(changeGameButton);
+  hideElement(classicCharacters);
+  hideElement(spicyCharacters);
+  hideElement(chooseFighter);
+  hideElement(result);
+  viewElement(chooseGame);
+  viewElement(rulesSection);
+}
+
+function viewElement(element) {
+  element.classList.remove("hidden");
+}
+
+function hideElement(element) {
+  element.classList.add("hidden");
 }
