618c21c33fdbccf037090cf0417bef35a5153289
Danielle Sweeny <dsweeny1@gmail.com>
2022-06-14 14:15:06 -0600

    Update conditional

--- game.js
+++ game.js
@@ -17,25 +17,40 @@ class Game {
 
   determineWinner() {
     if (this.human.humanFighter === this.computer.computerFighter) {
-    this.winner = "tie";
-  } else if (
-    (this.human.humanFighter === "rock" && this.computer.computerFighter === "scissors") ||
-    (this.human.humanFighter === "paper" && this.computer.computerFighter === "rock") ||
-    (this.human.humanFighter === "scissors" &&
-    this.computer.computerFighter === "paper") ||
-    (this.human.humanFighter === "rock" && this.computer.computerFighter === "mushroom") ||
-    (this.human.humanFighter === "paper" && this.computer.computerFighter === "flower") ||
-    (this.human.humanFighter === "scissors" && this.computer.computerFighter === "mushroom") ||
-    (this.human.humanFighter === "mushroom" && this.computer.computerFighter === "paper") ||
-    (this.human.humanFighter === "mushroom" && this.computer.computerFighter === "flower") ||
-    (this.human.humanFighter === "flower" && this.computer.computerFighter === "scissors") ||
-    (this.human.humanFighter === "flower" && this.computer.computerFighter === "rock")
-    ) {
-    this.winner = 'Human';
-    this.human.wins++
+      this.winner = "tie";
+    } else if (this.human.humanFighter === "rock" && this.computer.computerFighter === "scissors") {
+      this.winner = 'Human';
+      this.human.wins++
+    } else if (this.human.humanFighter === "paper" && this.computer.computerFighter === "rock") {
+      this.winner = 'Human';
+      this.human.wins++
+    } else if (this.human.humanFighter === "scissors" && this.computer.computerFighter === "paper") {
+      this.winner = 'Human';
+      this.human.wins++
+    } else if (this.human.humanFighter === "rock" && this.computer.computerFighter === "mushroom") {
+      this.winner = 'Human';
+      this.human.wins++
+    } else if (this.human.humanFighter === "paper" && this.computer.computerFighter === "flower") {
+      this.winner = 'Human';
+      this.human.wins++
+    } else if (this.human.humanFighter === "scissors" && this.computer.computerFighter === "mushroom") {
+      this.winner = 'Human';
+      this.human.wins++
+    } else if (this.human.humanFighter === "mushroom" && this.computer.computerFighter === "paper") {
+      this.winner = 'Human';
+      this.human.wins++
+    } else if (this.human.humanFighter === "mushroom" && this.computer.computerFighter === "flower") {
+      this.winner = 'Human';
+      this.human.wins++
+    } else if (this.human.humanFighter === "flower" && this.computer.computerFighter === "scissors") {
+      this.winner = 'Human';
+      this.human.wins++
+    } else if (this.human.humanFighter === "flower" && this.computer.computerFighter === "rock") {
+      this.winner = 'Human';
+      this.human.wins++
     } else {
-    this.winner = 'Computer';
-    this.computer.wins++
+      this.winner = 'Computer';
+      this.computer.wins++
     }
   }
 }
